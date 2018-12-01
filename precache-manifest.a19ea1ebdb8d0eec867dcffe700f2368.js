55a29986989143694d979187c80abbc483b11aa8
jskogelin <jonathan.skogelin@gmail.com>
2018-12-01 12:07:19 +0100

    Updates

---
+++ precache-manifest.a19ea1ebdb8d0eec867dcffe700f2368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8583053fa7d15d806c20540bd5d32145",
+    "url": "/jskogelin/julkalender/static/media/bil.8583053f.jpg"
+  },
+  {
+    "revision": "fd3cd1bba4bba6b23bc7",
+    "url": "/jskogelin/julkalender/static/js/runtime~main.fd3cd1bb.js"
+  },
+  {
+    "revision": "ae32d4d23a1af63ca623",
+    "url": "/jskogelin/julkalender/static/js/main.ae32d4d2.chunk.js"
+  },
+  {
+    "revision": "2aa3550c7ad0cff2f51a",
+    "url": "/jskogelin/julkalender/static/js/1.2aa3550c.chunk.js"
+  },
+  {
+    "revision": "ae32d4d23a1af63ca623",
+    "url": "/jskogelin/julkalender/static/css/main.3c61ee4f.chunk.css"
+  },
+  {
+    "revision": "5b88834d8aca307053fd367104b06012",
+    "url": "/jskogelin/julkalender/index.html"
+  }
+];
