b46e040903edc42d5b1fa4f69af54d4ac3159488
jskogelin <jonathan.skogelin@gmail.com>
2018-12-05 19:13:28 +0100

    get windows every few seconds

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import dragscroll from 'dragscroll';
 import './App.css';
 import Lucka from './Lucka.js';
 import Info from './info.js';
+import windows from './info.json'
 import makeSnow from './particles'
+import 'whatwg-fetch'
 
 // makeSnow()
 
 class App extends Component {
+  state = {
+    windows
+  }
+
+  constructor() {
+    super()
+    setInterval(() => {
+      fetch('https://raw.githubusercontent.com/jskogelin/julkalender/master/src/info.json')
+        .then(response => {
+          return response.json()
+        })
+        .then(windows => {
+          this.setState({
+            windows
+          })
+        })
+    }, 2000)
+  }
 
   render() {
     return (
       <div className="App">
         <div>
-        {Info.map((luckaInfo, index) => {
+        {this.state.windows.map((luckaInfo, index) => {
           return <Lucka
             key={index}
             x={luckaInfo.x}
